679df9287bba023e3caf2e284aabb1e017d0eeb0
wangrui <1522468667@qq.com>
2023-08-21 14:56:24 +0800

    feat: 修改配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,6 +25,12 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "prettier/prettier": "error",
+    "prettier/prettier": [
+      "error",
+      {
+        "singleQuote": true,
+        "parser": "flow"
+      }
+    ]
   },
 };
--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,58 +1,42 @@
-import { defineComponent, PropType, toRefs } from "vue";
-import "uno.css";
-export type ISize = "small" | "medium" | "large";
-export type IColor =
-  | "black"
-  | "gray"
-  | "red"
-  | "yellow"
-  | "green"
-  | "blue"
-  | "indigo"
-  | "purple"
-  | "pink";
-export type IconType =
-  | "search"
-  | "edit"
-  | "check"
-  | "message"
-  | "star-off"
-  | "delete"
-  | "add"
-  | "share";
+import { defineComponent, PropType, toRefs } from 'vue';
+import 'uno.css';
+export type ISize = 'small' | 'medium' | 'large';
+export type IColor = 'black' | 'gray' | 'red' | 'yellow' | 'green' | 'blue' | 'indigo' | 'purple' | 'pink';
+export type IconType = 'search' | 'edit' | 'check' | 'message' | 'star-off' | 'delete' | 'add' | 'share';
 export const props = {
   color: {
     type: String as PropType<IColor>,
-    default: "blue", // 设定默认颜色
+    default: 'blue', // 设定默认颜色
   },
   icon: {
-    type: String as PropType<IconType | "">,
+    type: String as PropType<IconType | ''>,
+    default:''
   },
   // 新增
   size: {
     type: String as PropType<ISize>,
-    default: "medium",
+    default: 'medium',
   },
 };
 export default defineComponent({
-  name: "SButton",
+  name: 'SButton',
   props,
   setup(props, { slots }) {
     const size = {
       small: {
-        x: "2",
-        y: "1",
-        text: "sm",
+        x: '2',
+        y: '1',
+        text: 'sm',
       },
       medium: {
-        x: "3",
-        y: "1.5",
-        text: "base",
+        x: '3',
+        y: '1.5',
+        text: 'base',
       },
       large: {
-        x: "4",
-        y: "2",
-        text: "lg",
+        x: '4',
+        y: '2',
+        text: 'lg',
       },
     };
     return () => (
@@ -73,12 +57,8 @@ export default defineComponent({
              py-${size[props.size].y}
              `}
       >
-        {props.icon !== "" ? (
-          <i class={`i-ic-baseline-${props.icon} p-3`}></i>
-        ) : (
-          ""
-        )}
-        {slots.default ? slots.default() : ""}
+        {props.icon != '' ? <i class={`i-ic-baseline-${props.icon} p-3`}></i> : ''}
+        {slots.default ? slots.default() : ''}
       </button>
     );
   },
